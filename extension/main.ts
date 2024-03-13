bed265965e1d17a8d11bf14564051f9be7cd25e1
lilezek <devezek@gmail.com>
2024-03-13 12:15:53 +0100

    feat: updated the main example

--- extension/main.ts
+++ extension/main.ts
@@ -1,19 +1,20 @@
 import { BrowsingContent } from "./browser/chrome.js";
 import { KeyCodes } from "../contentScripts/interact.js";
-import llm_selector from "./AI/llm_selector.js";
+import chat from "./chat/chat.js";
+import { sleep } from "../contentScripts/utils.js";
 
 const ctx = new BrowsingContent();
+
+chat.writeAssistantMessage('Hello!');
 
 document.getElementById('exec')!.addEventListener('click', async () => {
     const tab = await ctx.getCurrentTab();
     await tab.navigateTo('https://web.telegram.org');
     
-    const source = await tab.getSource();
-    
-    for await (const res of llm_selector.findXPath(source, 'Telegram', 'Alfredo Rubio')) {
-        await res.save();
-        tab.setLastElementXPath(res.xpath);
-    }
-
+    await tab.findElement('Alfredo Rubio');
     await tab.clickElement();
+    await sleep(1000);
+
+    await tab.findElement('Write a message bar');
+    await tab.sendKeysToElement('Hello, Alfredo!', KeyCodes.Enter);    
 });
