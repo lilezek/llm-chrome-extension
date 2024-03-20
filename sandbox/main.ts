a5ada56777c2b4cc49ce07d3f239274751348995
lilezek <devezek@gmail.com>
2024-03-20 21:49:37 +0100

    fix: missing Key global with the key codes in the sandbox environment

--- sandbox/main.ts
+++ sandbox/main.ts
@@ -1,9 +1,15 @@
 import { Tab } from "./runInExtension.js";
+import { KeyCodes } from "../contentScripts/interact.js";
+
+// Expose the KeyCodes object to the window
+// This is important given that the generated code will use keys such as "Key.ENTER" from the global scope.
+(window as any).Key = KeyCodes;
 
 window.addEventListener('message', (event) => {
     const { data } = event;
     const { type, code, args } = data;
     if (type == "eval") {
-        eval(code)(Tab, ...args);
+        const f = eval(code) as Function;
+        f.call(window, Tab, ...args);
     }
 });
