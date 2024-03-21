9f5e99bc20ea6e18a75a782ccfaefd00e84bf83f
lilezek <devezek@gmail.com>
2024-03-21 11:21:16 +0100

    feat: improved task generation and parsing

--- extension/tasks/task.ts
+++ extension/tasks/task.ts
@@ -1,9 +1,9 @@
-import { Tab } from "../browser/browser.js";
 import { SandboxContext } from "../sandbox/context.js";
+import { LocalStorage } from "../storage/localstorage.js";
 
 // TODO: replace this regex with a parser, won't work if one of the argument is a function, for example
 // async function name(f: () => void) ...
-const functionHeaderRE = /(async)?\s*function\s*([a-zA-Z0-9]+)\s*\(([^)]*)\)\s*/s;
+const functionHeaderRE = /(async)?\s*function\s*([a-zA-Z0-9]+)\s*\(([^)]*)\)\s*:([^{]*)/s;
 const bodyRE = /{(.*)}/s;
 
 interface Argument {
@@ -20,6 +20,33 @@ export class Task {
         private code: string,
     ) { }
 
+    private toJavascript() {
+        const argNames = this.args.map(arg => arg.name);
+        return `async function ${this.name}(${["tab", ...argNames].join(", ")}) {
+            ${this.code}
+        }`;
+    }
+
+    private prepareAllTasksContext() {
+        let code: string[] = [];
+        for (const task of Task.iterator()) {
+            code = [...code, task.toJavascript()];
+        }
+        return code.join('\n\n');
+    }
+
+    static * iterator() {
+        for (const file of LocalStorage.iterator()) {
+            const key = file.getKey().split('task ')[1];
+            if (key) {
+                const task = file.getJSON(Task);
+                if (task) {
+                    yield task;
+                }
+            }
+        }
+    }
+
     static fromCode(code: string) {
         code = code.trim();
         const header = code.match(functionHeaderRE);
@@ -27,12 +54,14 @@ export class Task {
             throw new Error('Invalid function header');
         }
 
-        const [, , name, args] = header;
+        const [, , name, args, retType] = header;
 
         const argsList = args.split(',').map(arg => {
             const [name, type] = arg.trim().split(':');
-            return { name, type: type ?? 'any' };
-        });
+            return { name: name.trim(), type: type.trim() ?? 'any' };
+        })
+        // Filter the tab argument to avoid duplicates
+        .filter(arg => arg.name != "tab");
 
         const body = code.match(bodyRE);
         if (!body) {
@@ -43,19 +72,47 @@ export class Task {
 
         return new Task(
             `Task generated from code: ${code}`,
-            name,
+            name.trim(),
             argsList,
-            'any',
-            codeBody,
+            retType.trim() ?? 'Promise<any>',
+            codeBody.trim(),
         );
+    }
+
+    static fromJSON(json: any) {
+        return new Task(
+            json.description,
+            json.name,
+            json.args,
+            json.retValue,
+            json.code,
+        );
+    }
+
+    static getTasksInTypescriptDefinition() {
+        let code: string[] = [];
+        for (const task of Task.iterator()) {
+            const argNames = task.args.map(arg => `${arg.name}: ${arg.type}`);
+            code = [...code, `async function ${task.name}(${["tab: TabContext", ...argNames].join(', ')}): Promise<${task.retValue}>;`];
+        }
+        return code.join('\n');
     }
 
     run(sandbox: SandboxContext, ...args: string[]) {
         const argNames = this.args.map(arg => arg.name);
-          const code = `(async (${(["tab", ...argNames]).join(", ")}) => {
+        const code = `(async (${["tab", ...argNames].join(", ")}) => {
             ${this.code}
         })`;
 
-        sandbox.eval(code, ...args);
+        return sandbox.eval(`${this.prepareAllTasksContext()}\n${code}`, ...args);
+    }
+
+    save(overwrite = false) {
+        // save to storage
+        if (LocalStorage.exists(`task ${this.name}`) && !overwrite) {
+            // TODO: ask the user for a new name
+            throw new Error(`Task with name ${this.name} already exists`);
+        }
+        new LocalStorage(`task ${this.name}`).setJSON(this);
     }
 }
